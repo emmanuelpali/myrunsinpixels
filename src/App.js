019cb6442465bb6e9ba8b44f4ce820afe972b597
emmanuelpali <palinendir@gmail.com>
2023-11-02 10:21:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import Gallery from './Components/Gallery/Gallery';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className="container text-center">
+        <h1>My Runs in pixels</h1>
+        <Gallery />
+      </div>
+    </>
+    
   );
 }
 
